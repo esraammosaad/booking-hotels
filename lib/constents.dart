2bd4a85a203a9c3b6487997c1cb35312a41dc45b
esraa mosaad <esraa.m.mosaad@gmail.com>
2023-09-20 07:51:50 +0300

    prepare the project

---
+++ lib/constents.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor=Color(0xffF8A13C);
+const Color kSecondaryColor=   Color(0x33F8A13C);
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+
+
+    );
+  }
+}
